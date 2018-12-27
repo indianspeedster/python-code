5aa7a1c0d1c20a1736e0dcb03cbd9cf4b6e6a5c0
indianspeedster <38083203+indianspeedster@users.noreply.github.com>
2018-12-27 15:11:05 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,30 @@
+#song_name
+name = 'jai ho' #string
+#movie name
+m_name = 'slumdog millionaire' #string
+#music company
+company = 'motion picture' #string
+genre = 'hip-hop' #string
+#length is in minutes
+length = 51 #int
+#ratings are in star
+Ratings = 51.5  #float
+singer = 'A.R RAHMAN' #string
+label = 't-series' #string
+#other singers
+featuring = 'sukhwinder singh , tanvi shah' #string
+# printing all the variables
+print (name) #printing song name
+print (m_name) #printing movie name
+print (company) #printing company name
+print (genre) #printing genre
+print (length) #printing length
+print (Ratings) #printing rating
+print (singer) #printing singer name
+print (label) # printing label
+print (featuring) #printing featuring
+"""
+i have tried to enter all the characteristics of my fav song
+Date = 27-15-2018
+Thank you
+"""
